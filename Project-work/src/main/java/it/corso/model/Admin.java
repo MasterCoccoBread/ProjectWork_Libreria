5c080a9283f1d1ce6ab479760b114361ceb1d703
FrancescoSdellamonica <francescosaverio.monica@gmail.com>
2023-04-12 15:11:06 +0200

    ModelFRA

---
+++ Project-work/src/main/java/it/corso/model/Admin.java
@@ -0,0 +1,16 @@
+package it.corso.model;
+
+public class Admin {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	} 
+	
+	// private Anagrafica id_anagrafica
+}
---
+++ Project-work/src/main/java/it/corso/model/Anagrafica.java
@@ -0,0 +1,63 @@
+package it.corso.model;
+
+
+
+public class Anagrafica {
+	
+	private int id; 
+	private String ragioneSociale; 
+	private String indirizzo; 
+	private String localita; 
+	private String provincia; 
+	private String cap; 
+	private String telefono;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRagioneSociale() {
+		return ragioneSociale;
+	}
+	public void setRagioneSociale(String ragioneSociale) {
+		this.ragioneSociale = ragioneSociale;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getLocalita() {
+		return localita;
+	}
+	public void setLocalita(String localita) {
+		this.localita = localita;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getCap() {
+		return cap;
+	}
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	} 
+	
+	
+	
+	// private Profilo id_profilo 
+	
+	
+	
+}

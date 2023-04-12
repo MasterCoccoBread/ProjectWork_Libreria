126f7c6c309546dacf996c18fff84703aa6698cb
Petrach95 <daniele.petrach@gmail.com>
2023-04-12 17:11:34 +0200

    17:11

--- Project-work/src/main/java/it/corso/model/Genere.java
+++ Project-work/src/main/java/it/corso/model/Genere.java
@@ -1,9 +1,21 @@
 package it.corso.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "generi")
 public class Genere {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(name = "descrizione")
 	private String descrizione;
 
 	public int getId() {

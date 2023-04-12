704c28f7b5e2da5d7e1add460afc7e988d5d80fb
MasterCoccoBread <mitchbob02@gmail.com>
2023-04-12 12:09:23 +0200

    First Commit

---
+++ src/main/java/it/corso/ProjectWorkLibreriaApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkLibreriaApplication.class, args);
+	}
+
+}

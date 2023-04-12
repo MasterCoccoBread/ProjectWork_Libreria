24879d62932a24593deeed04e0f95f362d978391
FrancescoSdellamonica <francescosaverio.monica@gmail.com>
2023-04-12 17:09:31 +0200

    Adjustment

--- Project-work/src/main/java/it/corso/model/Anagrafica.java
+++ Project-work/src/main/java/it/corso/model/Anagrafica.java
@@ -1,16 +1,26 @@
 package it.corso.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Entity
+@Table ("anagrafica")
 public class Anagrafica {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id; 
+	
+	@Column
 	private String nome;
+	@Column
 	private String cognome;
+	@Column
 	private String telefono;
-	
-	
-		
 		
 	public int getId() {
 		return id;
--- Project-work/src/main/java/it/corso/model/Autore.java
+++ Project-work/src/main/java/it/corso/model/Autore.java
@@ -1,9 +1,20 @@
 package it.corso.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table ("autori")
 public class Autore {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column
 	private String nome;
 
 	public int getId() {

e18985f89fdbbabcfb13d6f54a151c103b4d9a24
Petrach95 <daniele.petrach@gmail.com>
2023-04-12 15:10:47 +0200

    Parziale model

---
+++ Project-work/src/main/java/it/corso/model/Evento.java
@@ -0,0 +1,47 @@
+package it.corso.model;
+
+import java.time.LocalDate;
+
+public class Evento {
+
+	private int id;
+	
+	private String descrizione;
+
+	private LocalDate dataInizio;
+	
+	private LocalDate dataFine;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public LocalDate getDataInizio() {
+		return dataInizio;
+	}
+
+	public void setDataInizio(LocalDate dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+
+	public LocalDate getDataFine() {
+		return dataFine;
+	}
+
+	public void setDataFine(LocalDate dataFine) {
+		this.dataFine = dataFine;
+	}
+	
+}
--- Project-work/src/main/java/it/corso/model/Libro.java
+++ Project-work/src/main/java/it/corso/model/Libro.java
@@ -2,4 +2,62 @@ package it.corso.model;
 
 public class Libro {
 
+	private int id;
+	
+	private String titolo;
+	
+	private String descrizione;
+	
+	//private Tipologia tipologia;
+	
+	//private Genere genere;
+	
+	private double prezzo;
+	
+	private String immagine;
+
+	//private Autore autore;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public String getImmagine() {
+		return immagine;
+	}
+
+	public void setImmagine(String immagine) {
+		this.immagine = immagine;
+	}
+	
+	
+	
 }

355accc82b9982e2b27bf5930711499d83d91e92
Petrach95 <daniele.petrach@gmail.com>
2023-04-19 12:04:38 +0200

    evento pronto

--- Project-work/src/main/java/it/corso/controller/LibriController.java
+++ Project-work/src/main/java/it/corso/controller/LibriController.java
@@ -1,7 +1,5 @@
 package it.corso.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import it.corso.model.Libro;
 import it.corso.service.LibroService;
 import it.corso.service.PrenotazioneService;

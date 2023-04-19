e4b8fa60ad2ac8e1a7e20d5cc5ebc8dcd1eeffee
Petrach95 <daniele.petrach@gmail.com>
2023-04-19 15:12:23 +0200

    li

--- Project-work/src/main/java/it/corso/controller/LibriController.java
+++ Project-work/src/main/java/it/corso/controller/LibriController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import it.corso.model.Genere;
 import it.corso.model.Libro;
+import it.corso.service.GenereService;
 import it.corso.service.LibroService;
 import it.corso.service.PrenotazioneService;
 
@@ -18,12 +20,18 @@ public class LibriController {
 	@Autowired
 	private LibroService libroService;
 	@Autowired
+	private GenereService genereService;
+	@Autowired
 	private PrenotazioneService prenotazioneService;
 	
+	private Genere genere;
 	@GetMapping
 	private String getPage(Model model) {
 		List<Libro> libri = libroService.getLibri();
+		List<Genere> generi = genereService.getGeneri();
 		model.addAttribute("libri", libri);
+		model.addAttribute("generi", generi);
+		model.addAttribute("totale", true);
 		return "CatalogoLibri";
 	}
 	
@@ -36,5 +44,17 @@ public class LibriController {
 		String ticket = prenotazioneService.generaTicket(idLibro, tipoPrenotazione);
 		prenotazioneService.registraPrenotazione(ticket, idAnagrafica, idLibro, tipoPrenotazione);
 		return "redirect:/libri";
+	}
+	
+	@GetMapping("/generelibro")
+	public String getGenereLibro(
+			@RequestParam("idGenere") int idGenere,
+			Model model) {
+		
+		genere = genereService.getGenereById(idGenere);
+		List<Libro> genereLibri = genere.getLibri();
+		model.addAttribute("genereLibri", genereLibri);
+		model.addAttribute("totale", false);
+		return "CatalogoLibri"  ;
 	}
 }
